cd561fb05ee99cc995c97ee93028017754787265
Natalie P <cyuubiapps@gmail.com>
2021-02-19 03:02:22 -0500

    Remove BeatSaber hook library.

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -2,5 +2,5 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/libchroma.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_12.so", "./libs/arm64-v8a/libcustom-json-data.so", "./bmbfmod.json" -DestinationPath "./chroma_v0.1.0.zip" -Update
+    Compress-Archive -Path "./libs/arm64-v8a/libchroma.so", "./libs/arm64-v8a/libcustom-json-data.so", "./bmbfmod.json" -DestinationPath "./chroma_v0.1.0.zip" -Update
 }
