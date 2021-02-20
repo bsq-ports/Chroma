554ac91320fb96f3d06b4a9f3e38b9b31b713d80
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-02-19 21:10:59 -0400

    Clean up and separate code

--- src/hooks/LightSwitchEventEffect.cpp
+++ src/hooks/LightSwitchEventEffect.cpp
@@ -4,46 +4,15 @@
 #include "colorizer/LightColorizer.hpp"
 #include "custom-types/shared/types.hpp"
 #include "custom-types/shared/macros.hpp"
-#include "GlobalNamespace/LightSwitchEventEffect.hpp"
 #include "UnityEngine/WaitForEndOfFrame.hpp"
+#include "LightSwitchEventEffect.hpp"
 
 using namespace Chroma;
 using namespace GlobalNamespace;
 using namespace UnityEngine;
 using namespace System::Collections;
 
-DECLARE_CLASS_INTERFACES(Il2CppNamespace, WaitThenStartEnumerator, "System", "Object", sizeof(Il2CppObject),
-    il2cpp_utils::GetClassFromName("System.Collections", "IEnumerator"),
- 
-    DECLARE_CTOR(ctor, LightSwitchEventEffect *instance, BeatmapEventType eventType);
- 
-    DECLARE_INSTANCE_FIELD(Il2CppObject*, current);
-    DECLARE_INSTANCE_FIELD(bool, hasWaited);
- 
-    DECLARE_INSTANCE_FIELD(LightSwitchEventEffect*, instance);
-    DECLARE_INSTANCE_FIELD(BeatmapEventType, eventType);
- 
-    DECLARE_OVERRIDE_METHOD(bool, MoveNext, il2cpp_utils::FindMethod("System.Collections", "IEnumerator", "MoveNext"));
-    DECLARE_OVERRIDE_METHOD(Il2CppObject*, get_Current, il2cpp_utils::FindMethod("System.Collections", "IEnumerator", "get_Current"));
-    DECLARE_OVERRIDE_METHOD(void, Reset, il2cpp_utils::FindMethod("System.Collections", "IEnumerator", "Reset"));
- 
-    REGISTER_FUNCTION(WaitThenStartEnumerator,
-        getLogger().debug("Registering WaitThenStartEnumerator!");
- 
-        REGISTER_FIELD(current);
-        REGISTER_FIELD(hasWaited);
- 
-        REGISTER_FIELD(instance);
-        REGISTER_FIELD(eventType);
- 
-        REGISTER_METHOD(ctor);
- 
-        REGISTER_METHOD(MoveNext);
-        REGISTER_METHOD(get_Current);
-        REGISTER_METHOD(Reset);
-    )
-)
- 
+
 DEFINE_CLASS(Il2CppNamespace::WaitThenStartEnumerator);
  
 void Il2CppNamespace::WaitThenStartEnumerator::ctor(LightSwitchEventEffect *instance, BeatmapEventType eventType) {
