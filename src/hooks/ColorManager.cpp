c531cd1825a425fa92e1659e8fdb6684d61fc078
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-02-24 15:23:24 -0400

    Finish ColorManager

--- src/hooks/ColorManager.cpp
+++ src/hooks/ColorManager.cpp
@@ -5,6 +5,7 @@
 #include "colorizer/NoteColorizer.hpp"
 #include "GlobalNamespace/ColorManager.hpp"
 #include "GlobalNamespace/ColorType.hpp"
+#include "GlobalNamespace/SaberType.hpp"
 
 using namespace GlobalNamespace;
 using namespace Chroma;
@@ -21,6 +22,20 @@ MAKE_HOOK_OFFSETLESS(ColorManager_ColorForType, UnityEngine::Color, ColorManager
     return ColorManager_ColorForType(self, type);
 }
 
+MAKE_HOOK_OFFSETLESS(ColorManager_EffectsColorForSaberType, UnityEngine::Color, ColorManager* self, SaberType type) {
+    auto color = self->ColorForSaberType(type);
+
+    float h;
+    float s;
+    float ignored;
+
+    UnityEngine::Color::RGBToHSV(color, h, s, ignored);
+
+    return UnityEngine::Color::HSVToRGB(h, s, 1);
+}
+
 void Chroma::Hooks::ColorManager() {
     INSTALL_HOOK_OFFSETLESS(getLogger(), ColorManager_ColorForType, il2cpp_utils::FindMethodUnsafe("", "ColorManager", "ColorForType", 1));
+    INSTALL_HOOK_OFFSETLESS(getLogger(), ColorManager_EffectsColorForSaberType, il2cpp_utils::FindMethodUnsafe("", "ColorManager", "EffectsColorForSaberType", 1));
+
 }
