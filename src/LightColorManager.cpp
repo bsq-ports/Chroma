532c7ade98764f9b73412533040bfcc8fe5b3446
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-03-12 12:20:42 -0400

    Reduce crashes, still not fixed completely

--- src/LightColorManager.cpp
+++ src/LightColorManager.cpp
@@ -24,96 +24,97 @@ void Chroma::LightColorManager::ColorLightSwitch(MonoBehaviour* monobehaviour, C
     if(customDataWrapper && customDataWrapper->value) {
         getLogger().debug("JSON data 1");
 
-        //TODO: FIX THE NULLPTR HERE THAT OCCURS
-        // NO PATTERN OR CAUSE RECOGNIZED YET
-
 
         rapidjson::Value *dynData = beatmapEventData->customData->value;
         if (il2cpp_functions::class_is_assignable_from(monobehaviour->klass, classof(LightSwitchEventEffect *))) {
             auto *lightSwitchEventEffect = reinterpret_cast<LightSwitchEventEffect *>(monobehaviour);
 
-            if (dynData->HasMember("_lightID")) {
-                getLogger().debug("JSON data 2");
-                rapidjson::Value &lightIdData = dynData->FindMember("_lightID")->value;
-                std::vector<ILightWithId *> lights = LightColorizer::GetLights(lightSwitchEventEffect);
-                int lightCount = lights.size();
+            // TODO: FIX OR REMOVE! This helps avoid a crash with CJD's rapidjson allocation.
+            if (!dynData->Empty()) {
 
-                if (lightIdData.IsInt() || lightIdData.IsInt64()) {
-                    auto lightIdLong = lightIdData.GetInt();
-                    if (lightCount > lightIdLong) {
+                if (dynData->HasMember("_lightID")) {
+                    getLogger().debug("JSON data 2");
+                    rapidjson::Value &lightIdData = dynData->FindMember("_lightID")->value;
+                    std::vector<ILightWithId *> lights = LightColorizer::GetLights(lightSwitchEventEffect);
+                    int lightCount = lights.size();
+
+                    if (lightIdData.IsInt() || lightIdData.IsInt64()) {
+                        auto lightIdLong = lightIdData.GetInt();
+                        if (lightCount > lightIdLong) {
+                            std::vector<ILightWithId *> overrideLights;
+
+                            overrideLights.push_back(lights[lightIdLong]);
+
+                            OverrideLightWithIdActivation = std::make_optional(overrideLights);
+                        }
+                    } else {
+                        // It's a list
+                        auto lightIDobjects = lightIdData.GetObject();
+                        std::vector<int> lightIDArray;
+
+
+                        for (auto &lightId : lightIDobjects) {
+                            lightIDArray.push_back(lightId.value.GetInt());
+                        }
+
                         std::vector<ILightWithId *> overrideLights;
 
-                        overrideLights.push_back(lights[lightIdLong]);
+                        for (auto lightId : lightIDArray) {
+                            if (lightCount > lightId) {
+                                overrideLights.push_back(lights[lightId]);
+                            }
+                        }
 
                         OverrideLightWithIdActivation = std::make_optional(overrideLights);
                     }
-                } else {
-                    // It's a list
-                    auto lightIDobjects = lightIdData.GetObject();
-                    std::vector<int> lightIDArray;
-
-
-                    for (auto &lightId : lightIDobjects) {
-                        lightIDArray.push_back(lightId.value.GetInt());
-                    }
-
-                    std::vector<ILightWithId *> overrideLights;
-
-                    for (auto lightId : lightIDArray) {
-                        if (lightCount > lightId) {
-                            overrideLights.push_back(lights[lightId]);
-                        }
-                    }
-
-                    OverrideLightWithIdActivation = std::make_optional(overrideLights);
                 }
-            }
 
-            if (dynData && dynData->HasMember("_propID")) {
-                getLogger().debug("JSON data 3");
-                rapidjson::Value &propIDData = dynData->FindMember("_propID")->value;
+                if (dynData && dynData->HasMember("_propID")) {
+                    getLogger().debug("JSON data 3");
+                    rapidjson::Value &propIDData = dynData->FindMember("_propID")->value;
 
-                std::unordered_map<int, std::vector<ILightWithId *>> lights = LightColorizer::GetLightsPropagationGrouped(
-                        lightSwitchEventEffect);
-                int lightCount = lights.size();
+                    std::unordered_map<int, std::vector<ILightWithId *>> lights = LightColorizer::GetLightsPropagationGrouped(
+                            lightSwitchEventEffect);
+                    int lightCount = lights.size();
 
-                getLogger().debug("Prop id data is");
-                PrintJSONValue(propIDData);
+                    getLogger().debug("Prop id data is");
+                    PrintJSONValue(propIDData);
 
-                if (propIDData.IsInt64() || propIDData.IsInt()) {
-                    auto propIdLong = propIDData.GetInt();
-                    if (lightCount > propIdLong) {
-                        OverrideLightWithIdActivation = std::make_optional(lights[propIdLong]);
-                    }
-                } else {
-                    // It's a list
-                    auto propIDobjects = propIDData.GetObject();
-                    std::vector<int> propIDArray;
+                    if (propIDData.IsInt64() || propIDData.IsInt()) {
+                        auto propIdLong = propIDData.GetInt();
+                        if (lightCount > propIdLong) {
+                            OverrideLightWithIdActivation = std::make_optional(lights[propIdLong]);
+                        }
+                    } else {
+                        // It's a list
+                        auto propIDobjects = propIDData.GetObject();
+                        std::vector<int> propIDArray;
 
 
-                    for (auto &lightId : propIDobjects) {
-                        propIDArray.push_back(lightId.value.GetInt());
-                    }
+                        for (auto &lightId : propIDobjects) {
+                            propIDArray.push_back(lightId.value.GetInt());
+                        }
 
-                    std::vector<ILightWithId *> overrideLights;
+                        std::vector<ILightWithId *> overrideLights;
 
-                    for (auto propId : propIDArray) {
-                        if (lightCount > propId) {
-                            for (auto l : lights[propId]) {
-                                overrideLights.push_back(l);
+                        for (auto propId : propIDArray) {
+                            if (lightCount > propId) {
+                                for (auto l : lights[propId]) {
+                                    overrideLights.push_back(l);
+                                }
                             }
                         }
+
+                        OverrideLightWithIdActivation = std::make_optional(overrideLights);
                     }
-
-                    OverrideLightWithIdActivation = std::make_optional(overrideLights);
                 }
-            }
 
-            //gradientObject
-            // FIXME, THIS IS VERY TEMP!!
-            if (dynData->HasMember("_lightGradient")) {
-                color = ChromaGradientController::AddGradient(&dynData->FindMember("_lightGradient")->value,
-                                                              beatmapEventData->type, beatmapEventData->time);
+                //gradientObject
+                // FIXME, THIS IS VERY TEMP!!
+                if (dynData->HasMember("_lightGradient")) {
+                    color = ChromaGradientController::AddGradient(&dynData->FindMember("_lightGradient")->value,
+                                                                  beatmapEventData->type, beatmapEventData->time);
+                }
             }
         }
 
