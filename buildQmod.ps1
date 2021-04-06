4902d6b4a398396c1415764c38eda67519aad5d4
Kaitlyn <cyuubiapps@gmail.com>
2021-04-05 21:35:19 -0400

    Update buildQmod.ps1

--- buildQmod.ps1
+++ buildQmod.ps1
@@ -3,5 +3,6 @@
 
 if ($?) {
     Compress-Archive -Path "./mod.json", "./libs/arm64-v8a/libchroma.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so", "./libs/arm64-v8a/libcustom-json-data.so", "./libs/arm64-v8a/libquestui.so", "./libs/arm64-v8a/libbs-utils.so" -DestinationPath "./chroma_v0.1.0.zip" -Update
+    Remove-Item "./chroma_v0.1.0.qmod"
     Rename-Item "./chroma_v0.1.0.zip" "./chroma_v0.1.0.qmod"
 }
