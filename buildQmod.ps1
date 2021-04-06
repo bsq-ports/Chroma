f07128aa44411c45fcef0a3285a2c40af2b33f2a
Kaitlyn <cyuubiapps@gmail.com>
2021-04-05 22:18:30 -0400

    Include old BSHook versions

--- buildQmod.ps1
+++ buildQmod.ps1
@@ -2,7 +2,7 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./mod.json", "./libs/arm64-v8a/libchroma.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so", "./libs/arm64-v8a/libcustom-json-data.so", "./libs/arm64-v8a/libquestui.so", "./libs/arm64-v8a/libbs-utils.so" -DestinationPath "./chroma_v0.1.0.zip" -Update
+    Compress-Archive -Path "./mod.json", "./libs/arm64-v8a/libchroma.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so", "./libbeatsaber-hook_1_2_3.so", "./libbeatsaber-hook_1_1_6.so", "./libs/arm64-v8a/libcustom-json-data.so", "./libs/arm64-v8a/libquestui.so", "./libs/arm64-v8a/libbs-utils.so" -DestinationPath "./chroma_v0.1.0.zip" -Update
     Remove-Item "./chroma_v0.1.0.qmod"
     Rename-Item "./chroma_v0.1.0.zip" "./chroma_v0.1.0.qmod"
 }
