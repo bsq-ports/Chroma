0870ac62776f3ddaf9d3c032e2eb6f9ea8784e19
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-04-19 22:44:44 -0400

    Fix environment override, though legacy environment won't work

--- src/hooks/SceneTransition/SceneTransitionHelper.cpp
+++ src/hooks/SceneTransition/SceneTransitionHelper.cpp
@@ -10,7 +10,9 @@
 #include "GlobalNamespace/BeatmapDataSO.hpp"
 #include "GlobalNamespace/BeatmapEnvironmentHelper.hpp"
 #include "GlobalNamespace/EnvironmentInfoSO.hpp"
+#include "Chroma.hpp"
 
+#include "utils/ChromaUtils.hpp"
 
 #include <cstdlib>
 
@@ -27,14 +29,29 @@ void SceneTransitionHelper::Patch(GlobalNamespace::IDifficultyBeatmap* customBea
 void SceneTransitionHelper::Patch(GlobalNamespace::IDifficultyBeatmap* customBeatmapData, OverrideEnvironmentSettings*& overrideEnvironmentSettings) {
 
 
-
     bool chromaRequirement = SceneTransitionHelper::BasicPatch(customBeatmapData);
-    if (chromaRequirement && getChromaConfig().environmentEnhancementsEnabled.GetValue() && false)
-
+    if (chromaRequirement && getChromaConfig().environmentEnhancementsEnabled.GetValue() && customBeatmapData)
         //TODO: Actually implement this once info.dat information is retrievable
         // && customBeatmapData->customData && customBeatmapData->customData->value && customBeatmapData->customData->value->HasMember("_environmentRemoval"))
-        {
-        overrideEnvironmentSettings = nullptr;
+    {
+        auto il2cppObject = reinterpret_cast<Il2CppObject*>(customBeatmapData);
+        getLogger().debug("Beatmap klass: %s", il2cpp_utils::ClassStandardName(il2cppObject->klass).c_str());
+        if (ASSIGNMENT_CHECK(classof(CustomJSONData::CustomBeatmapData*), il2cppObject->klass)) {
+            auto customBeatmapDataCustom = reinterpret_cast<CustomJSONData::CustomBeatmapData *>(customBeatmapData);
+
+            if (customBeatmapDataCustom->customData) {
+                auto dynData = customBeatmapDataCustom->customData->value;
+
+                if (dynData) {
+                    auto it = dynData.value().get().FindMember(ENVIRONMENT);
+                    auto it2 = dynData.value().get().FindMember(ENVIRONMENTREMOVAL);
+
+                    if (it != dynData.value().get().MemberEnd() || it2 != dynData.value().get().MemberEnd()) {
+                        overrideEnvironmentSettings = nullptr;
+                    }
+                }
+            }
+        }
     }
 }
 
