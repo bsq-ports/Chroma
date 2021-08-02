2dae0e5a9b69c8117d8c0ede2d4877f48960bcd8
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-08-02 11:58:16 -0400

    Preparing for release of sekrit project.

--- buildQmod.ps1
+++ buildQmod.ps1
@@ -2,7 +2,7 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./mod.json", "./libs/arm64-v8a/libchroma.so", "./libs/arm64-v8a/libbeatsaber-hook_1_3_5.so", "./libs/arm64-v8a/libcustom-json-data.so", "./libs/arm64-v8a/libquestui.so", "./libs/arm64-v8a/libcustom-types.so" -DestinationPath "./chroma_v0.1.0.zip" -Update
-    Remove-Item "./chroma_v0.1.0.qmod"
-    Rename-Item "./chroma_v0.1.0.zip" "./chroma_v0.1.0.qmod"
+    Compress-Archive -Path "./mod.json", "./libs/arm64-v8a/libchroma.so", "./libs/arm64-v8a/libbeatsaber-hook_2_2_4.so", "./libs/arm64-v8a/libcustom-json-data.so", "./libs/arm64-v8a/libquestui.so", "./libs/arm64-v8a/libcustom-types.so" -DestinationPath "./chroma_v0.1.0.zip" -Update
+    Remove-Item "./chroma_v2.4.10.qmod"
+    Rename-Item "./chroma_v0.1.0.zip" "./chroma_v2.4.10.qmod"
 }
