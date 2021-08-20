3e1fba51e8039523e4e42a8d573b367f18e3be8a
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-08-20 18:42:04 -0400

    This is version bump magic

--- buildQmod.ps1
+++ buildQmod.ps1
@@ -2,7 +2,7 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./mod.json", "./libs/arm64-v8a/libchroma.so", "./libs/arm64-v8a/libbeatsaber-hook_2_2_4.so", "./libs/arm64-v8a/libcustom-json-data.so", "./libs/arm64-v8a/libquestui.so", "./libs/arm64-v8a/libcustom-types.so" -DestinationPath "./chroma_v0.1.0.zip" -Update
-    Remove-Item "./chroma_v2.4.10.qmod"
-    Rename-Item "./chroma_v0.1.0.zip" "./chroma_v2.4.10.qmod"
+    Compress-Archive -Path "./mod.json", "./libs/arm64-v8a/libchroma.so", "./libs/arm64-v8a/libbeatsaber-hook_2_2_5.so", "./libs/arm64-v8a/libcustom-json-data.so", "./libs/arm64-v8a/libtracks.so", "./libs/arm64-v8a/libquestui.so", "./libs/arm64-v8a/libcustom-types.so" -DestinationPath "./chroma_v0.1.0.zip" -Update
+    Remove-Item "./chroma_v2.4.10q2.qmod"
+    Rename-Item "./chroma_v0.1.0.zip" "./chroma_v2.4.10q2.qmod"
 }
