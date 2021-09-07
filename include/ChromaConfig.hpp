e8c10f312431ce162c86ff51956e91741ae735f8
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-09-07 17:13:55 -0400

    Fix typo

--- include/ChromaConfig.hpp
+++ include/ChromaConfig.hpp
@@ -9,7 +9,7 @@
 DECLARE_CONFIG(ChromaConfig,
 
         CONFIG_VALUE(PrintEnvironmentEnhancementDebug, bool, "PrintEnvironmentEnhancementDebug", false);
-        CONFIG_VALUE(customColorEventsEnabled, bool, "Custom Color Events", true, "Disables custom colors for Chroma maps. In other words, disables Chroma");
+        CONFIG_VALUE(customColorEventsEnabled, bool, "Custom Color Events", true, "Disables custom colors for Chroma maps. In other words, disables Chroma.");
         CONFIG_VALUE(environmentEnhancementsEnabled, bool, "Environment Enhancements", true, "Toggles whether Chroma should modify the environment as specified by a map.");
 
 
