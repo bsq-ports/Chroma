42c8278f1a079d3420ed8006509b8014bf14782b
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-06-04 23:01:23 -0400

    Nicer code

--- include/utils/ChromaUtils.hpp
+++ include/utils/ChromaUtils.hpp
@@ -192,24 +192,24 @@ template <typename T> inline static constexpr std::optional<T*> ptrToOpt(T* t) {
 /// </summary>
 static UnityEngine::Mesh* CreateTriangleMesh() {
   static SafePtrUnity<UnityEngine::Mesh> mesh;
-  if(!mesh)
-  {
-    ArrayW<Sombrero::FastVector3> vertices =
-        std::initializer_list<Sombrero::FastVector3>({ { -0.5f, -0.5f, 0 }, { 0.5f, -0.5f, 0 }, { 0.0f, 0.5f, 0 } });
+  if(mesh) return mesh.ptr();
 
-    ArrayW<Sombrero::FastVector2> uv = std::initializer_list<Sombrero::FastVector2>({ { 0, 0 }, { 1, 0 }, { 0.5f, 1 } });
+  ArrayW<Sombrero::FastVector3> vertices =
+      std::initializer_list<Sombrero::FastVector3>({ { -0.5f, -0.5f, 0 }, { 0.5f, -0.5f, 0 }, { 0.0f, 0.5f, 0 } });
 
-    ArrayW<int> triangles = { 0, 1, 2 };
+  ArrayW<Sombrero::FastVector2> uv = std::initializer_list<Sombrero::FastVector2>({ { 0, 0 }, { 1, 0 }, { 0.5f, 1 } });
 
-    mesh = UnityEngine::Mesh::New_ctor();
-    mesh->set_vertices(reinterpret_cast<Array<UnityEngine::Vector3>*>(vertices.convert()));
-    mesh->set_uv(reinterpret_cast<Array<UnityEngine::Vector2>*>(uv.convert()));
-    mesh->set_triangles(triangles);
+  ArrayW<int> triangles = { 0, 1, 2 };
 
-    mesh->RecalculateBounds();
-    mesh->RecalculateNormals();
-    mesh->RecalculateTangents();
-  }
+  mesh = UnityEngine::Mesh::New_ctor();
+  mesh->set_vertices(reinterpret_cast<Array<UnityEngine::Vector3>*>(vertices.convert()));
+  mesh->set_uv(reinterpret_cast<Array<UnityEngine::Vector2>*>(uv.convert()));
+  mesh->set_triangles(triangles);
+
+  mesh->RecalculateBounds();
+  mesh->RecalculateNormals();
+  mesh->RecalculateTangents();
+  
   return mesh.ptr();
 }
 
