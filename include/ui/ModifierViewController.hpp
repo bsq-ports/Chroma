05aa6da2e8ce46024960b72b65473def6ab683f1
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-07-09 13:50:38 -0400

    Implement Future's UI

--- include/ui/ModifierViewController.hpp
+++ include/ui/ModifierViewController.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "HMUI/ViewController.hpp"
 #include "UnityEngine/MonoBehaviour.hpp"
 
 #include <utility>
@@ -10,15 +11,16 @@
 #include "custom-types/shared/macros.hpp"
 #include "main.hpp"
 
-namespace Chroma {
-namespace UIUtils {
-UnityEngine::Sprite* configToIcon(ConfigUtils::ConfigValue<bool> const& configValue);
-} // namespace UIUtils
-} // namespace Chroma
+#include "bsml/shared/macros.hpp"
 
-DECLARE_CLASS_CODEGEN(Chroma, ModifierViewController, UnityEngine::MonoBehaviour,
+DECLARE_CLASS_CODEGEN(Chroma, ModifierViewController, HMUI::ViewController,
 
                       DECLARE_INSTANCE_METHOD(void, DidActivate, bool first);
+                      DECLARE_OVERRIDE_METHOD(void, DidActivate2, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidActivate", 3), bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+
+                      DECLARE_BSML_PROPERTY(bool, environmentEnhancementsEnabled);
+                      DECLARE_BSML_PROPERTY(bool, customColorEventsEnabled);
+                      DECLARE_BSML_PROPERTY(bool, customNoteColors);
 
                       public
                       : DECLARE_SIMPLE_DTOR();)
--- src/ui/ModifierViewController.cpp
+++ src/ui/ModifierViewController.cpp
@@ -1,10 +1,14 @@
 #include "ui/ModifierViewController.hpp"
 
+#include "ChromaConfig.hpp"
 #include "UnityEngine/RectOffset.hpp"
 #include "UnityEngine/Resources.hpp"
 #include "UnityEngine/Sprite.hpp"
 
+#include "UnityEngine/Transform.hpp"
+#include "bsml/shared/BSML.hpp"
 #include "main.hpp"
+#include "assets.hpp"
 
 using namespace Chroma;
 using namespace UnityEngine::UI;
@@ -12,28 +16,35 @@ using namespace UnityEngine;
 
 DEFINE_TYPE(Chroma, ModifierViewController)
 
-UnityEngine::Sprite* UIUtils::configToIcon(ConfigUtils::ConfigValue<bool> const& configValue) {
-  static const std::unordered_map<ConfigUtils::ConfigValue<bool> const*, std::string> spriteNameMap = {
-    { &getChromaConfig().environmentEnhancementsEnabled, "GlobalIcon" },
-    { &getChromaConfig().customColorEventsEnabled, "LightIcon" },
-    { &getChromaConfig().customNoteColors, "DisappearingArrows" }
-  };
-  static std::unordered_map<std::string_view, UnityEngine::Sprite*> spriteMap;
-
-  auto const& name = spriteNameMap.at(&configValue);
-
-  auto& sprite = spriteMap[name];
-
-  if (UnityW(sprite) == nullptr) {
-    sprite = Resources::FindObjectsOfTypeAll<Sprite*>()->First(
-                                       [&name](Sprite* x) { return x->get_name() == name; });
+void Chroma::ModifierViewController::DidActivate(bool firstActivation) {
+  if (firstActivation) {
+    BSML::parse_and_construct(IncludedAssets::ui_bsml, transform, this);
   }
-
-  return sprite;
 }
 
-void Chroma::ModifierViewController::DidActivate(bool first) {
-
-  if (first) {
+void Chroma::ModifierViewController::DidActivate2(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+  if (firstActivation) {
+    BSML::parse_and_construct(IncludedAssets::ui_bsml, transform, this);
   }
+}
+
+bool Chroma::ModifierViewController::get_environmentEnhancementsEnabled() {
+    return getChromaConfig().environmentEnhancementsEnabled.GetValue();
+}
+void Chroma::ModifierViewController::set_environmentEnhancementsEnabled(bool value) {
+    getChromaConfig().environmentEnhancementsEnabled.SetValue(value, true);
+}
+
+bool Chroma::ModifierViewController::get_customNoteColors() {
+    return getChromaConfig().customNoteColors.GetValue();
+}
+void Chroma::ModifierViewController::set_customNoteColors(bool value) {
+    getChromaConfig().customNoteColors.SetValue(value, true);
+}
+
+bool Chroma::ModifierViewController::get_customColorEventsEnabled() {
+    return getChromaConfig().customColorEventsEnabled.GetValue();
+}
+void Chroma::ModifierViewController::set_customColorEventsEnabled(bool value) {
+    getChromaConfig().customColorEventsEnabled.SetValue(value, true);
 }
